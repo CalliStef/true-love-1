6fc8be3045632e5ef63bab35608cc0f869a0f83e
Callista Stefanie Taswin <callistastefanie@gmail.com>
2023-05-01 14:51:35 -0700

    fixed session name requirement

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require("express")
 const bodyParser = require("body-parser")
-const session = require("cookie-session")
+const cookieSession = require("cookie-session")
 const app = express()
 
 const server = require('http').createServer(app)
